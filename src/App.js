0f211ae210afbbba77680f2639d090a3e4bb4fb3
Neil Hall <neil@thingsyettobe.co.uk>
2019-02-03 23:45:57 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReact } from '@fortawesome/free-brands-svg-icons'
+import { faAnchor } from '@fortawesome/free-solid-svg-icons'
 
-class App extends Component {
-  render() {
+library.add(faReact, faAnchor)
+
+
+function AnarchyListItem({ className, component: Component = "li", ...props}) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <Component
+            className={["anarchist-list-item", className].join(" ")}
+            {...props}
+        />
+    )
 }
 
-export default App;
+let listData = [
+    {id:1, name: 'Vandalism'},
+    {id:2, name: 'Lists'},
+    {id:3, name: 'Irony'},
+    {id:4, name: 'Lists'},
+    {id:5, name: 'Repitition'}
+]
+
+export default function App () {
+    return (
+      <div className="center">
+        <h1>
+            <p>
+                <FontAwesomeIcon icon={['fab', 'react']} size='2x' style={{"color":"#61dafb"}}/>
+                &nbsp;React Hooks&nbsp;
+                <FontAwesomeIcon icon="anchor" size='2x'/>
+            </p>
+            Things I Hate:-
+        </h1>
+        <h2>
+            <ol type = "1">
+                {
+                    listData.map(item => (<AnarchyListItem key={item.id}>{item.name}</AnarchyListItem>))
+                }
+            </ol>
+              <ol type = "A">
+                <AnarchyListItem className="test">Inconsitency</AnarchyListItem>
+              </ol>
+        </h2>
+      </div>
+    );
+}
+
+
